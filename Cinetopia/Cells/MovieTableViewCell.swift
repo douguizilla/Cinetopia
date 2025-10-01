02d6e6abfb81e3f392b76f4e1850b8858faf74dc
Douglas Gomes <douglasgommez@gmail.com>
2025-09-30 22:33:15 -0300

    feat: add cell

--- Cinetopia/Cells/MovieTableViewCell.swift
+++ Cinetopia/Cells/MovieTableViewCell.swift
@@ -12,8 +12,9 @@ class MovieTableViewCell: UITableViewCell {
     private lazy var moviePosterImageView: UIImageView = {
         let imageView = UIImageView(image: UIImage.avatar)
         imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
         return imageView
     }()
     
@@ -23,6 +24,7 @@ class MovieTableViewCell: UITableViewCell {
         label.font = .systemFont(ofSize: 20, weight: .bold)
         label.textColor = .white
         label.text = "Avatar"
+        label.numberOfLines = 0
         return label
     }()
     
@@ -34,6 +36,12 @@ class MovieTableViewCell: UITableViewCell {
         label.text = "Lançamento: 18/12/2009"
         return label
     }()
+    
+    func configureCell(with movie: Movie) {
+        movieTitleLabel.text = movie.title
+        moviePosterImageView.image = UIImage(named: movie.image)
+        movieReleaseDateLabel.text = "Lançamento: \(movie.releaseData)"
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -71,8 +79,9 @@ class MovieTableViewCell: UITableViewCell {
                 moviePosterImageView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -16),
                 moviePosterImageView.widthAnchor.constraint(equalToConstant: 100),
                 
-                movieTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+                movieTitleLabel.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -16),
                 movieTitleLabel.leadingAnchor.constraint(equalTo: moviePosterImageView.trailingAnchor, constant: 16),
+                movieTitleLabel.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
                 
                 movieReleaseDateLabel.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 8),
                 movieReleaseDateLabel.leadingAnchor.constraint(equalTo: moviePosterImageView.trailingAnchor, constant: 16),
---
+++ Cinetopia/ViewController/MovieDetailsViewController.swift
@@ -0,0 +1,94 @@
+//
+//  MovieDetailsViewController.swift
+//  Cinetopia
+//
+//  Created by Douglas Gomes on 30/09/25.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+    
+    var movie: Movie
+    
+    init(movie: Movie) {
+        self.movie = movie
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var movieTitleLabel : UILabel = {
+        let label = UILabel()
+        label.text = movie.title
+        label.font = .systemFont(ofSize: 24, weight: .bold)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var movieImage : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: movie.image))
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 12
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private lazy var movieRateLabel : UILabel = {
+        let label = UILabel()
+        label.text = "Classificação dos usuários: \(movie.rate)"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private lazy var synopsisLabel : UILabel = {
+        let label = UILabel()
+        label.text = movie.synopsis
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+        addSubviews()
+        setupConstraints()
+    }
+
+    private func addSubviews() {
+        [movieTitleLabel, movieImage, movieRateLabel, synopsisLabel].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate(
+            [
+                movieTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+                movieTitleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                                
+                movieImage.topAnchor.constraint(equalTo: movieTitleLabel.bottomAnchor, constant: 16),
+                movieImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+                movieImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+                movieImage.heightAnchor.constraint(equalToConstant: 200),
+                
+                movieRateLabel.topAnchor.constraint(equalTo: movieImage.bottomAnchor, constant: 16),
+                movieRateLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+                
+                synopsisLabel.topAnchor.constraint(equalTo: movieRateLabel.bottomAnchor, constant: 16),
+                synopsisLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+                synopsisLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            ]
+        )
+    }
+}
