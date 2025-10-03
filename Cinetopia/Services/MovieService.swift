47b2957fd217b1a5fdabbd9be3529368e2092a5f
Douglas Gomes <douglasgommez@gmail.com>
2025-10-02 21:04:29 -0300

    refact: refatoração para usar async/await

--- Cinetopia/Services/MovieService.swift
+++ Cinetopia/Services/MovieService.swift
@@ -7,33 +7,29 @@
 
 import Foundation
 
+enum MovieServiceError: Error {
+    case invalidURL
+    case invalidResponse
+    case decodingError
+}
+
 struct MovieService {
-    func getMovies(
-        completion: @escaping ([Movie]?) -> Void
-    ) {
+    func getMovies() async throws -> [Movie] {
         let urlString = "https://my-json-server.typicode.com/alura-cursos/movie-api/movies"
         guard let url = URL(string: urlString) else {
-            completion(nil)
-            return
+            throw MovieServiceError.invalidURL
         }
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data,
-                  let httpResponse = response as? HTTPURLResponse,
-                  httpResponse.statusCode == 200 else {
-                completion(nil)
-                return
-            }
-            
-            do{
-                let movies = try JSONDecoder().decode([Movie].self, from: data)
-                completion(movies)
-            }catch{
-                completion(nil)
-                print(error.localizedDescription)
-            }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw MovieServiceError.invalidResponse
         }
         
-        task.resume()
+        do{
+            return try JSONDecoder().decode([Movie].self, from: data)
+        }catch{
+            throw MovieServiceError.decodingError
+        }
     }
 }
